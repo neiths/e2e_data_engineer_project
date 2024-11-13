80a76ecede49742d035f1159cbf40e992abcbd4c
Thien Huynh <49941781+neiths@users.noreply.github.com>
2024-11-13 08:53:30 +0000

    add: DAGs config in kafka_stream.py

--- dags/kafka_stream.py
+++ dags/kafka_stream.py
@@ -40,12 +40,31 @@ def stream_data():
     import json
     from kafka import KafkaProducer
     import time
+    import logging
 
-    data = get_data()
-    data = format_data(data)
+    producer = KafkaProducer(bootstrap_servers=['broker:29092'], max_block_ms=5000)
+    curr_time = time.time()
 
-    producer = KafkaProducer(bootstrap_servers=['localhost:9092'], max_block_ms=5000)
+    while True:
+        if time.time() > curr_time + 60: #1 minute
+            break
+        try:
+            res = get_data()
+            res = format_data(res)
 
-    producer.send('users_created', json.dumps(data).encode('utf-8'))
+            producer.send('users_created', json.dumps(res).encode('utf-8'))
+        except Exception as e:
+            logging.error(f'An error occured: {e}')
+            continue
 
-stream_data()
+with DAG(
+        'user_automation',
+         default_args=default_args,
+         schedule_interval='@daily',
+         catchup=False
+         ) as dag:
+
+    streaming_task = PythonOperator(
+        task_id='stream_data_from_api',
+        python_callable=stream_data
+    )
